0a6fd6a9072d567f3ce8521cf0a6afc50a0aa641
Akintunde Segun <Akintundeayofesegun@gmail.com>
2020-04-12 05:03:49 -0400

    oddeven passed

---
+++ oddeven.py
@@ -0,0 +1,22 @@
+
+# Given an integer, , perform the following conditional actions:
+#
+# If n is odd, print Weird
+# If n is even and in the inclusive range of 2 to 5 , print Not Weird
+# If n is even and in the inclusive range of 6 to 20 , print Weird
+# If  n is even and greater than 20 , print Not Weird
+
+
+n = input("enter the number: ")
+
+print(int(n) % 2)
+
+if int(n) % 2 > 0:
+    print("Weird")
+else:
+    if int(n) <= 5:
+        print('Not Weird')
+    elif int(n) <= 20:
+        print("Weird")
+    else:
+        print("Not Weird")
